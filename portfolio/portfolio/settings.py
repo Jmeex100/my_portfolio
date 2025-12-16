843dba7938e0a3ce5fe43a26d5d602b31faae39d
Jmeex100 <110308816+Jmeex100@users.noreply.github.com>
2025-12-16 10:49:38 -0800

    fin

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -73,26 +73,26 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',       # Replace with your MySQL database name
-        'USER': '',          # Replace with your MySQL username
-        'PASSWORD': '',  # Replace with your MySQL password
-        'HOST': 'localhost',                # Or your DB server IP
-        'PORT': '3306',                     # Default MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydb',       # Replace with your MySQL database name
+#         'USER': 'root',          # Replace with your MySQL username
+#         'PASSWORD': '',  # Replace with your MySQL password
+#         'HOST': 'localhost',                # Or your DB server IP
+#         'PORT': '3306',                     # Default MySQL port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
